d59727cfae5bd0b9909c52392f460d1cb847342c
Matthew Pagan <mastapegs01@gmail.com>
2023-12-23 14:43:46 -0500

    adjust cdk stack for amanda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -6,9 +6,9 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new StaticSite(this, "StaticSite", {
+    new StaticSite(this, "AmandasCardsStaticSite", {
       domainName: "matthewpagan.com",
-      siteSubDomain: "example",
+      siteSubDomain: "amandas-card",
     });
   }
 }
