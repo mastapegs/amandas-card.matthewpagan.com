c8715b08763d822c54e31d754e4f645c4c739af6
Matthew Pagan <mastapegs01@gmail.com>
2023-12-23 16:50:59 -0500

    adjusted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>Happy Birthday Amanda!!!</h1>;
-}
+const App = (): JSX.Element => {
+  return (
+    <>
+      <h1>Happy Birthday Amanda!!!</h1>
+      <p>{`I hope you enjoy this special day honey <3`}</p>
+      <p>
+        There are so many positive effects you have on our family. You plan
+        activities for our kids, even though they are CRAZY every single time.
+        You make time to get presents for our kids' bus drivers and teachers,
+        you invite over their friends, even if its difficult.
+      </p>
+      <p>You are selfless.</p>
+      <p>
+        Without you, this family would be lost, so I wanted to tell you that you
+        are the most important person on the planet to Noah, Kenzie, Nyah,
+        Benny, and I. Happy 31st Birthday, and I hope this coming year, is our
+        year to shine.
+      </p>
+      <p>{`I love you so much <3`}</p>
+    </>
+  );
+};
 
 export default App;
